bbea620620defe68659b863de402887b1820f16f
Amiralitolouei1388 <amttgml@gmail.com>
2025-03-24 17:13:52 +0330

    Update urls.py

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,7 +6,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<int>/', views.topic, name='topic'),
+   path('topics/<slug:slug>/', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
     path('new_entry/<slug:slug>/', views.new_entry, name='new_entry'),
 ]
